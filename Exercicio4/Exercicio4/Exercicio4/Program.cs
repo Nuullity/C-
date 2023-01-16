597e4f06228966575192279f92cf03fee533da06
Nuullity <marcos.venancio058@gmail.com>
2023-01-16 11:29:08 -0300

    ex 4 - A Org

---
+++ Exercicio4/Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+
+
+
+        Console.WriteLine("Entre com seu nome completo:");
+        String nome = Console.ReadLine();
+
+        Console.WriteLine("Quantos quartos tem sua casa ?");
+        int quartos = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Qual o preço pela sua vida ?");
+        double preco = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Entre seu ultimo nome, idade e altura (mesma linha):");
+        String[] vet = Console.ReadLine().Split(' ');
+
+        string ultNome = vet[0];
+        int idade = int.Parse(vet[1]);
+        double altura = double.Parse(vet[2]);
+
+
+        Console.WriteLine("Informarções adquiridas:");
+        Console.WriteLine();
+        Console.WriteLine("nome: " + nome);
+        Console.WriteLine("quartos: " + quartos);
+        Console.WriteLine("preço: " + preco.ToString("F2"));
+        Console.WriteLine("ultimo nome: " + ultNome);
+        Console.WriteLine("Sua idade: " + idade);
+        Console.WriteLine("Sua altura : " + altura.ToString("F2"));
+        Console.WriteLine("A Organização logo irá atras de voce, prepare-se!!");
+        Console.ReadLine();
+
+
+
+
+
+    }
+
+
+}
